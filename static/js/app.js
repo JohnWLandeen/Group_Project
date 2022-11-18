67b9ff81da2354750656fded08b5df9ee3790aa8
Albert Zeng <albertzeng@Albert-2.local>
2022-11-17 18:01:10 -0800

    relocated long link

--- static/js/app.js
+++ static/js/app.js
@@ -30,6 +30,8 @@ function buildTable(data) {
   });
 }
 
+var divElement = document.getElementById('viz1668658982710'); var vizElement = divElement.getElementsByTagName('object')[0]; if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '1016px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '1014px'; vizElement.style.maxHeight = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '1016px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '1014px'; vizElement.style.maxHeight = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.minWidth = '1016px'; vizElement.style.maxWidth = '100%'; vizElement.style.minHeight = '1550px'; vizElement.style.maxHeight = (divElement.offsetWidth * 1.77) + 'px'; } var scriptElement = document.createElement('script'); scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
 // 1. Create a variable to keep track of all the filters as an object.
 var filters = {};
 
